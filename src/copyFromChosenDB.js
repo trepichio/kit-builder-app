11cf541dbd5c42249d27616d07b9c3530211b53f
João Trepichio <trepichio@gmail.com>
2020-01-20 00:09:53 -0300

    copy chosen database to working directory

---
+++ src/copyFromChosenDB.js
@@ -0,0 +1,44 @@
+const logger = require('./logger')
+const fs = require('fs')
+const config = require('config')
+const path = require('path')
+const canAccess = require('./helpers/canAccess')
+
+
+module.exports = (data) => {
+  const { dirRootName, dirSysName, dirDBName, dirDBS, driverLetter } = config.get('Builder').builderConfig
+  // const { customerName, kitName, kitVersion, kitPrograms, test } = config.get('Builder').preparation
+  const { customerName, kitName, kitVersion, kitPrograms, test } = data
+  const { dbExts, dbName, dbNames, dbPath } = config.get('Builder').dbConfig
+
+  const dbsDir = path.join(driverLetter, dirRootName, dirDBS)
+  const dbWDir = path.join(driverLetter, dirRootName, dirDBName)
+  // const dbWDir = dbPath
+
+
+
+  const originalPath = path.join(dbsDir, `${kitName}-v${kitVersion}-DB`)
+  const workingPath = dbWDir
+  logger.info(`It will try to copy original DBs for ${kitName} to working folder`)
+  for (const dbName of dbNames[kitName]) {
+
+    for (const dbExt of dbExts) {
+      const dbSrc = path.join(originalPath, `${dbName}${dbExt}`)
+      const dbDest = path.join(workingPath, `${dbName}${dbExt}`)
+
+      if (canAccess(dbSrc)) {
+        try {
+          logger.info(`Copying ${dbSrc} to ${dbDest}`)
+          fs.copyFileSync(dbSrc, dbDest)
+          logger.info(`${dbSrc} copied successfully.`)
+          break;
+        } catch (error) {
+          logger.error(`Failed to copy ${dbSrc} to ${dbDest}. Error: ${error}`)
+        }
+      }
+
+    }
+
+  }
+
+}
