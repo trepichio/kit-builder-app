39a5cfe84e16cc17755cf7f69a131d10e49e1b32
João Trepichio <trepichio@gmail.com>
2020-01-25 16:16:46 -0300

    insert test parameter (do not delete files)

--- src/helpers/removeFiles.js
+++ src/helpers/removeFiles.js
@@ -1,11 +1,12 @@
 const findRemoveSync = require("find-remove")
 const logger = require('../logger')
 
-module.exports = ({ baseDir, mxLv, strFile, msgAlert, msgSuccess }) => {
-  console.log(msgAlert)
+module.exports = ({ baseDir, mxLv = 1, strFile = '', msgAlert = 'Files will be deleted now', msgSuccess = 'Deleted sucessfully' }) => {
+  logger.warn(msgAlert)
   const deletedFiles = findRemoveSync(baseDir, {
     maxLevel: mxLv,
-    files: strFile
+    files: strFile,
+    test: true
   })
   logger.info(msgSuccess, deletedFiles)
 }
