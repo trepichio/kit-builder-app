115f293cfb9e8c7c9cef4d0ae281dbef29c5c8c1
João Trepichio <trepichio@gmail.com>
2020-09-14 14:45:12 -0300

    format code: change quotes, add ; and indentation add docs comments

--- src/copyFromChosenDB.js
+++ src/copyFromChosenDB.js
@@ -1,44 +1,53 @@
-const logger = require('./logger')
-const fs = require('fs')
-const config = require('config')
-const path = require('path')
-const canAccess = require('./helpers/canAccess')
+const logger = require("./logger");
+const fs = require("fs");
+const config = require("config");
+const path = require("path");
+const canAccess = require("./helpers/canAccess");
 
-
-module.exports = (data) => {
-  const { dirRootName, dirSysName, dirDBName, dirDBS, driverLetter } = config.get('Builder').builderConfig
+/**
+ ** Copy database files for requested Kit to working DB folder
+ *
+ * @author João Trepichio
+ * @async
+ * @function copyFromChosenDB
+ * @param {Object} data - object that comes from task queue
+ */
+module.exports = async data => {
+  const {
+    dirRootName,
+    dirSysName,
+    dirDBName,
+    dirDBS,
+    driverLetter
+  } = config.get("Builder").builderConfig;
   // const { customerName, kitName, kitVersion, kitPrograms, test } = config.get('Builder').preparation
-  const { customerName, kitName, kitVersion, kitPrograms, test } = data
-  const { dbExts, dbName, dbNames, dbPath } = config.get('Builder').dbConfig
+  const { customerName, kitName, kitVersion, kitPrograms, test } = data;
+  const { dbExts, dbName, dbNames, dbPath } = config.get("Builder").dbConfig;
 
-  const dbsDir = path.join(driverLetter, dirRootName, dirDBS)
-  const dbWDir = path.join(driverLetter, dirRootName, dirDBName)
+  const dbsDir = path.join(driverLetter, dirRootName, dirDBS);
+  const dbWDir = path.join(driverLetter, dirRootName, dirDBName);
   // const dbWDir = dbPath
 
-
-
-  const originalPath = path.join(dbsDir, `${kitName}-v${kitVersion}-DB`)
-  const workingPath = dbWDir
-  logger.info(`It will try to copy original DBs for ${kitName} to working folder`)
+  const originalPath = path.join(dbsDir, `${kitName}-v${kitVersion}-DB`);
+  const workingPath = dbWDir;
+  logger.info(
+    `It will try to copy original DBs for ${kitName} to working folder`
+  );
   for (const dbName of dbNames[kitName]) {
-
     for (const dbExt of dbExts) {
-      const dbSrc = path.join(originalPath, `${dbName}${dbExt}`)
-      const dbDest = path.join(workingPath, `${dbName}${dbExt}`)
+      const dbSrc = path.join(originalPath, `${dbName}${dbExt}`);
+      const dbDest = path.join(workingPath, `${dbName}${dbExt}`);
 
-      if (canAccess(dbSrc)) {
+      if (await canAccess(dbSrc)) {
         try {
-          logger.info(`Copying ${dbSrc} to ${dbDest}`)
-          fs.copyFileSync(dbSrc, dbDest)
-          logger.info(`${dbSrc} copied successfully.`)
+          logger.info(`Copying ${dbSrc} to ${dbDest}`);
+          fs.copyFileSync(dbSrc, dbDest);
+          logger.info(`${dbSrc} copied successfully.`);
           break;
         } catch (error) {
-          logger.error(`Failed to copy ${dbSrc} to ${dbDest}. Error: ${error}`)
+          logger.error(`Failed to copy ${dbSrc} to ${dbDest}. Error: ${error}`);
         }
       }
-
     }
-
   }
-
-}
+};
