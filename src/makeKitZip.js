2b549838f0a885d948cdd3d4cbd4ec393ab209db
João Trepichio <trepichio@gmail.com>
2020-09-14 16:58:14 -0300

    add docs comment, format code

--- src/makeKitZip.js
+++ src/makeKitZip.js
@@ -1,20 +1,36 @@
-const AdmZip = require('adm-zip')
-const config = require('config')
-const logger = require('./logger')
-const path = require('path')
+const AdmZip = require("adm-zip");
+const config = require("config");
+const logger = require("./logger");
+const path = require("path");
 
-module.exports = (data) => {
-
-  const kitZip = new AdmZip()
+/**
+ ** Creates a zip file containing a customized Kit for deployment
+ *
+ * @author João Trepichio
+ * @function makeKitZip
+ * @param  {Object} data - task object from queue
+ */
+module.exports = data => {
+  const kitZip = new AdmZip();
 
   // const standardFolders = ['Acesso', 'Install', 'DB']
   const { standardFolders } = config.get('Builder').builderConfig
   // const { rootDir, customerName, kitPrograms, kitName, kitVersion, dateCreated } = config.get('Builder').preparation
-  const { rootDir, customerName, kitPrograms, kitName, kitVersion, dateCreated } = data
+  const {
+    rootDir,
+    customerName,
+    kitPrograms,
+    kitName,
+    kitVersion,
+    dateCreated
+  } = data;
+  let { validas } = data;
+  if (!validas) validas = [];
+  logger.info("TCL: validas", validas);
 
-  const opt = { year: 'numeric', month: 'numeric', day: 'numeric' };
-  const requestTime = new Date(dateCreated).toLocaleString('pt-br', opt)
-  logger.info(`makeKitZip -> requestTime ${requestTime}`)
+  const opt = { year: "numeric", month: "numeric", day: "numeric" };
+  const requestTime = new Date(dateCreated).toLocaleString("pt-br", opt);
+  logger.info(`makeKitZip -> requestTime ${requestTime}`);
 
 
   const kitProgramsPath = kitPrograms.map(program => `Sistemas\\${program}`)
@@ -26,14 +42,19 @@ module.exports = (data) => {
     logger.info(`makeKitZip -> ${folder} added to kitZip archive.`)
   }
 
-  const filename = `kit-${customerName}-${kitName}-V${kitVersion}-CR${requestTime}.zip`
-  const assetsFolder = path.resolve('../../kit-installer/installer/assets/')
+  const filename = `kit-${customerName}-${kitName}-V${kitVersion}-CR${requestTime}.zip`;
+  const assetsFolder = path.resolve("../../kit-installer/installer/assets/");
 
-  logger.info("makeKitZip -> Let's try to write the zip file on disk")
+  logger.info("makeKitZip -> Let's try to write the zip file on disk");
   try {
-    kitZip.writeZip(path.join(assetsFolder, filename))
-    logger.info(`makeKitZip -> Zip file ${filename} created successfully at ${assetsFolder}`)
+    kitZip.writeZip(path.join(assetsFolder, filename));
+    logger.info(
+      `makeKitZip -> Zip file ${filename} created successfully at ${assetsFolder}`
+    );
   } catch (error) {
-    logger.error(`makeKitZip -> Failed to write kitZip file for ${customerName}. Error: ${error}`)
+    logger.error(
+      `makeKitZip -> Failed to write kitZip file for ${customerName}. Error: ${error}`
+    );
+    throw error;
   }
-}
+};
