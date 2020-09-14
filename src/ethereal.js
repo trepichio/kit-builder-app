44db5ab975c19773851009498eb49a6c2c8bbea7
João Trepichio <trepichio@gmail.com>
2020-09-14 15:08:57 -0300

    format code, comment code, hard code some values handle error sending email

--- src/ethereal.js
+++ src/ethereal.js
@@ -23,11 +23,13 @@ module.exports = async (kit, filename) => {
     portServer
   } = config.get("Builder").repositoryConfig;
 
+  //** const kitDestination = path.join(ftpURL, rootFtp, dirFtpSuporte, dirFtpKits)
 
-module.exports = async (kit) => {
-  // Use at least Nodemailer v4.1.0
-  const nodemailer = require('nodemailer');
-  const logger = require('./logger')
+  const { driverLetter } = config.get("Builder").builderConfig;
+  // const kitDestination = `${driverLetter}\\${dirSuporte}\\${dirKits}\\${kit.customerName}\\${filename}`;
+  const kitDestination = `http://${ipServer}:${portServer}/1/`;
+  logger.info(`TCL: sendMail -> filename: ${filename}`);
+  logger.info(`TCL: kitDestination: ${kitDestination}`);
 
   // return await new Promise((resolve, reject) => {
 
@@ -39,8 +41,8 @@ module.exports = async (kit) => {
   //     return reject(err.message)
   //   }
 
-  //   logger.info('Credentials obtained, sending message...');
-  let account = await nodemailer.createTestAccount()
+  logger.info("Credentials obtained, sending message...");
+  let account = await nodemailer.createTestAccount();
 
   // Create a SMTP transporter object
   let transporter = nodemailer.createTransport({
@@ -53,19 +55,53 @@ module.exports = async (kit) => {
     }
   });
 
-  logger.info('Transport created...');
+  // let transporter = nodemailer.createTransport({
+  //   service: "gmail",
+  //   auth: {
+  //     user: "joao.trepichio@mbd.com.br",
+  //     pass: "try==:)84" // naturally, replace both with your real credentials or an application-specific password
+  //   }
+  // });
+
+  // let transporter = nodemailer.createTransport({
+  //   host: "https://mail.google.com/",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     type: "OAuth2",
+  //     clientId: "000000000000-xxx.apps.googleusercontent.com",
+  //     clientSecret: "XxxxxXXxX0xxxxxxxx0XXxX0"
+  //   }
+  // });
+
+  logger.info("Transport created...");
   // Message object
   let message = {
-    from: 'Suporte MBD <suporte@mbd.com.br>',
-    to: 'Analista <analista@mbd.com.br>',
-    subject: 'Kit is ready! ✔',
-    text: `You can download the generated kit ${kit.kitName} for ${kit.customerName} at ${kit.kitDestination}!!`,
-    html: `<p>You can <b>download</b> the generated kit <em>${kit.kitName}</em> for <b>${kit.customerName}</b> at <a href="#">${kit.kitDestination}</a> !!!!</p>`
+    from: "Suporte MBD <suporte@mbd.com.br>",
+    to: "João <joao.trepichio@mbd.com.br>",
+    subject: "Kit is ready! ✔",
+    text: `You can download the generated kit ${kit.kitName} for ${kit.customerName} at ${kitDestination}!!`,
+    html: `<p>You can <b>download</b> the generated kit <em>${kit.kitName}</em> for <b>${kit.customerName}</b> at <a href="${kitDestination}">${kitDestination}</a> !!!!</p>`
+    // html: `<p>You can <b>download</b> the generated kit <em>${kit.kitName}</em> for <b>${kit.customerName}</b> at <a href="file:///${kitDestination}">${kitDestination}</a> !!!!</p>`
+    // auth: {
+    //   user: "user@example.com",
+    //   refreshToken: "1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx",
+    //   accessToken: "ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x",
+    //   expires: 1484314697598
+    // }
   };
-  logger.info('Message created...');
+  logger.info("Message created...");
 
   // let sent = await new Promise((resv, rej) => {
-  let info = await transporter.sendMail(message)
+  try {
+    let info = await transporter.sendMail(message);
+    logger.info("Message sent: %s", info.messageId);
+    logger.info("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    return nodemailer.getTestMessageUrl(info);
+  } catch (error) {
+    logger.info("Failed to send e-mail. Error: %s", error);
+    throw error;
+  }
   // transporter.sendMail(message, (err, info) => {
   //   if (err) {
   //     logger.error('Error occurred. ' + err.message);
@@ -82,15 +118,15 @@ module.exports = async (kit) => {
   // })
   //   })
 
-  logger.info("Message sent: %s", info.messageId);
+  //** logger.info("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
-  logger.info("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  //** logger.info("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   // if (!!sent) resolve(sent)
   // else reject(sent)
 
   // })
   // })
-}
+};
