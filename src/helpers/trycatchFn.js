8f31819f4a438b7f96a939d3e566480e6bebc5b3
João Trepichio <trepichio@gmail.com>
2020-09-14 14:41:27 -0300

    add logger error message

--- src/helpers/trycatchFn.js
+++ src/helpers/trycatchFn.js
@@ -1,8 +1,10 @@
+const logger = require('../logger')
+
 module.exports = async (fn, ...params) => {
   try {
     return await fn(...params)
   } catch (err) {
-    console.log(err)
+    logger.error(err)
     return undefined // or whatever you want
   }
 }
