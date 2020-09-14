f7103259715656eb23d75c6a9dbcd05e7ecbf7df
João Trepichio <trepichio@gmail.com>
2020-09-14 14:42:55 -0300

    get true case path of file and add docs comment

--- src/backupConfigIniFiles.js
+++ src/backupConfigIniFiles.js
@@ -1,12 +1,23 @@
-const fs = require("fs")
-const logger = require("./logger")
-
-module.exports = path => {
-  logger.info(`Backing up original file in ${path} as Config.ini.bak`)
+const fs = require("fs");
+const logger = require("./logger");
+const { trueCasePathSync } = require("true-case-path");
+/**
+ ** Create .BAK file for provided filepath
+ *
+ * @author João Trepichio
+ * @param  {PathLike} path - a path to a directory
+ * @param  {String} filename - a filename with extension
+ */
+module.exports = async (path, filename) => {
   try {
-    fs.renameSync(`${path}/Config.ini`, `${path}/Config.ini.bak`)
-    logger.info(`Backup of ${path}\\Config.ini created successfully.`)
+    const truepath = trueCasePathSync(`${path}\\${filename}`);
+    logger.info(`Backing up original file as ${truepath}.bak`);
+    // fs.renameSync(`${path}/Config.ini`, `${path}/Config.ini.bak`)
+    fs.copyFileSync(truepath, `${truepath}.bak`);
+    // fs.renameSync(truepath, `${truepath}.bak`)
+    logger.info(`Backup of ${truepath} created successfully.`);
   } catch (error) {
-    logger.error(`Failed to backup Config.ini. Error: ${error}`)
+    logger.error(`Failed to backup Config.ini. Error: ${error}`);
+    throw error;
   }
-}
+};
